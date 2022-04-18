c3bc04bf46691688fd6741ced09d7fe833970fe1
MarDi66 <yohann.mtfpro@gmail.com>
2022-04-18 23:01:14 +0200

    Add UploadButton

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@apivideo/core";
+import { Button as UploadButton } from "@apivideo/upload-button"
 import { useIsomorphicLayoutEffect } from "@apivideo/utils";
 
 export default function Docs() {
@@ -9,6 +10,7 @@ export default function Docs() {
     <div>
       <h1>apivideo Documentation</h1>
       <Button>Click me</Button>
+      <UploadButton />
     </div>
   );
 }
