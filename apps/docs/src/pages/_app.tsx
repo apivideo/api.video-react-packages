9e6bf4267bdd0fdbc89146c9c54ac144cf9ddf10
MarDi66 <yohann.mtfpro@gmail.com>
2022-04-19 17:24:52 +0200

    feat(docs): CSS reset

---
+++ apps/docs/src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/reset.css'
+
+export default function App(props: any) {
+    const { Component, pageProps } = props
+    return <Component {...pageProps} />
+}
--- apps/docs/src/styles/index.ts
+++ apps/docs/src/styles/index.ts
@@ -2,7 +2,12 @@ import styled from 'styled-components'
 
 export const GlobalContainer = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  padding: 20px;
+  padding: 40px;
+  h1 {
+    font-size: 32px;
+    font-weight: 600;
+    margin-bottom: 30px;
+  }
 `
 
 export const DisplayContainer = styled.div`
@@ -14,7 +19,12 @@ export const PackageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 15px;
   padding: 20px;
   box-shadow: rgb(11 15 23 / 5%) 0px 1px 10px 0px, rgb(11 15 23 / 10%) 0px 1px 3px 0px;
   border-radius: 5px;
+  h3 {
+    font-size: 20px;
+    font-weight: 500;
+  }
 `
