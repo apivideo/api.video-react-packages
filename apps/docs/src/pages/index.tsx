d4a85ac5907421dd9ee9202c609f806fb0eebf7f
MarDi66 <yohann.mtfpro@gmail.com>
2022-04-26 17:08:13 +0200

    update(apps/docs): Remove unused

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,25 +1,16 @@
 import { UploadButton } from "@api.video/upload-button"
-import { List } from "@api.video/contents-list"
-import { useIsomorphicLayoutEffect } from "@api.video/utils"
 import * as S from '../styles'
 
 export default function Docs() {
-  useIsomorphicLayoutEffect(() => {
-    console.log("apivideo docs page");
-  }, []);
   return (
     <S.GlobalContainer>
       <h1>api.video React library ⚛️</h1>
       <S.DisplayContainer>
         <S.PackageContainer>
           <h3>{UploadButton.displayName}</h3>
-          <UploadButton uploadToken="to6l43T5QSnYZPS0dpCjPp3U">
+          <UploadButton uploadToken="YOUR_UPLOAD_TOKEN">
             Upload Button
           </UploadButton>
-        </S.PackageContainer>
-        <S.PackageContainer>
-          <h3>{List.displayName}</h3>
-          <List />
         </S.PackageContainer>
       </S.DisplayContainer>
     </S.GlobalContainer>
