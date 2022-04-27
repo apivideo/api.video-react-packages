8a0bbe62353602336f586a6f195538edc9bb6c7b
MarDi66 <yohann.mtfpro@gmail.com>
2022-04-27 18:14:34 +0200

    feat(packages/ui/upload-button): Add tests

---
+++ packages/ui/upload-button/src/specs/add.spec.ts
@@ -0,0 +1,7 @@
+import { add } from "../add";
+describe("add function", () => {
+   it("should add two number together", () => {
+      const result = add(10, 5);
+      expect(result).toBe(15);
+   });
+});
