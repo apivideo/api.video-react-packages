545fcd5a00bea1f93adc5f44d6cef40433d91d9e
MarDi66 <yohann.mtfpro@gmail.com>
2022-04-28 09:17:21 +0200

    feat(packages/ui/upload-button): Add tests

--- packages/ui/upload-button/src/specs/Button.spec.tsx
+++ packages/ui/upload-button/src/specs/Button.spec.tsx
@@ -2,12 +2,24 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/react'
+import { getByText, render } from '@testing-library/react'
 import { UploadButton } from '../Button'
 
 describe('<UploadButton />', () => {
   it('should display an upload button', () => {
     const { baseElement } = render(<UploadButton uploadToken='foo'>Upload Button</UploadButton>)
     expect(baseElement).toBeTruthy()
+  })
+
+  it('should have "Upload Button" for children', () => {
+    const { container } = render(<UploadButton uploadToken='foo'>Upload Button</UploadButton>)
+    expect(getByText(container, 'Upload Button')).toBeTruthy()
+  })
+
+  it('should have a hidden input', () => {
+    const { baseElement } = render(<UploadButton uploadToken='foo'>Upload Button</UploadButton>)
+    const input = baseElement.querySelector('input')
+    expect(input).toBeTruthy()
+    expect(input).toHaveProperty('hidden')
   })
 })
